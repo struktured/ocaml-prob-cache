418f148e5d60d65e94aaf07a55cb149dc57c1609
carm <carmelo.piccione@gmail.com>
2014-09-07 19:17:51 -0400

    probability cache work

--- lib/model.ml
+++ lib/model.ml
@@ -2,28 +2,44 @@ open Core.Std
 open Mpmw
 open Bin_prot
 open Bin_prot.Std
-
+open Bin_prot_utils
+(* TODO move into some other package *)
 let rec powerset = function
   | [] -> [[]]
   | h::t -> List.fold_left (powerset t) ~f:(fun xs t -> ((h::t)::t::xs)) ~init:[] ;;
 
-type 'a key = 'a list with bin_io
+type 'a events = 'a list with bin_io
 
 type value = int with bin_io
 
-type 'a t = (('a key), value) Cache.t
+type 'a t = {name:string;cache:(('a events), value) Cache.t}
 
-let count (model:'a t) elements = 
-  match_lwt Cache.get model elements with
+let create name key_serializer =
+  let value_serializer = Bin_prot_utils.create bin_read_value bin_write_value in
+  let cache = Cache.create 
+    ~key_serializer
+    ~value_serializer 
+    ~bucket:name 
+    () in {name;cache}
+
+let count ~(model:'a t) ~events = 
+  match_lwt Cache.get model.cache events with
   Some x -> Lwt.return x
   | None -> Lwt.return 0
- 
-let increment ~(model:'a t) ?(weight=1) ~(elements:'a key) =
-  lwt cnt = (count model elements) in 
-  Cache.put model elements (cnt + weight)
 
-let observe (model:'a t) (elements:'a list) =
-  List.iter (powerset elements) (fun set -> Lwt.ignore_result (increment model set));
+let conditional_prob ?(conditioned_on=[]) ~model ~events =
+  lwt event_count = count model events in
+  lwt given_cond_count = count model conditioned_on in
+  lwt cond_count = if given_cond_count = 0 && (not (List.is_empty conditioned_on)) then count model [] else Lwt.return(given_cond_count) in
+  if (cond_count = 0) then Lwt.return(Float.of_int 0) else
+  Lwt.return((Float.of_int event_count) /. (Float.of_int cond_count)) 
+
+let increment ~(model:'a t) ?(weight=1) ~(events:'a events) =
+  lwt cnt = (count model events) in 
+  Cache.put model.cache events (cnt + weight)
+
+let observe ~(model:'a t) ~(events:'a events) =
+  List.iter (powerset events) (fun set -> Lwt.ignore_result (increment model set));
  
   (* 
   let distance elem elem' model =
