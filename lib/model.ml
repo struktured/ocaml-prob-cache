e5d95e91c1827c33877bef548c25cbbd87f5131b
carm <carmelo.piccione@gmail.com>
2014-09-07 22:25:05 -0400

    use sets rather than lists, more encapsulation at same time with modules / functors

--- lib/model.ml
+++ lib/model.ml
@@ -3,18 +3,34 @@ open Mpmw
 open Bin_prot
 open Bin_prot.Std
 open Bin_prot_utils
+open Sexplib
+open Sexplib.Std
+
+
 (* TODO move into some other package *)
 let rec powerset = function
   | [] -> [[]]
   | h::t -> List.fold_left (powerset t) ~f:(fun xs t -> ((h::t)::t::xs)) ~init:[] ;;
 
-type 'a events = 'a list with bin_io
+module type EVENT = sig type t with bin_io, compare, sexp end 
 
-type value = int with bin_io
+module Events = functor (Event:EVENT) ->
+struct 
+  module EventSet = Core.Std.Set.Make_binable(Event)
+  include EventSet
+end
 
-type 'a t = {name:string;cache:(('a events), value) Cache.t}
 
-let create name key_serializer =
+module EventCache = functor (Event:EVENT) ->
+  struct
+type value = int with bin_io, compare, sexp
+
+module Events = Events(Event)
+
+type t = {name:string;cache:(Events.t, value) Cache.t}
+
+let create name =
+  let key_serializer = Bin_prot_utils.create Events.bin_read_t Events.bin_write_t in
   let value_serializer = Bin_prot_utils.create bin_read_value bin_write_value in
   let cache = Cache.create 
     ~key_serializer
@@ -22,25 +38,28 @@ let create name key_serializer =
     ~bucket:name 
     () in {name;cache}
 
-let count ~(model:'a t) ~events = 
+let count ~model ~events = 
   match_lwt Cache.get model.cache events with
   Some x -> Lwt.return x
   | None -> Lwt.return 0
 
-let conditional_prob ?(conditioned_on=[]) ~model ~events =
+let probability ?(conditioned_on=Events.empty) ~model ~(events:Events.t) =
   lwt event_count = count model events in
-  lwt given_cond_count = count model conditioned_on in
-  lwt cond_count = if given_cond_count = 0 && (not (List.is_empty conditioned_on)) then count model [] else Lwt.return(given_cond_count) in
+  lwt given_cond_count = count model conditioned_on in 
+  lwt cond_count = if given_cond_count = 0 && 
+    (not (Events.is_empty conditioned_on)) then count ~model ~events:Events.empty else Lwt.return(given_cond_count) in
   if (cond_count = 0) then Lwt.return(Float.of_int 0) else
   Lwt.return((Float.of_int event_count) /. (Float.of_int cond_count)) 
 
-let increment ~(model:'a t) ?(weight=1) ~(events:'a events) =
+let increment ~model ?(weight=1) ~(events:Events.t) =
   lwt cnt = (count model events) in 
   Cache.put model.cache events (cnt + weight)
 
-let observe ~(model:'a t) ~(events:'a events) =
-  List.iter (powerset events) (fun set -> Lwt.ignore_result (increment model set));
- 
+let observe ~model ~(events:Events.t) =
+  List.iter (powerset (Events.to_list events)) 
+    (fun set -> Lwt.ignore_result (increment model (Events.of_list set)));
+end
+
   (* 
   let distance elem elem' model =
    let numerator = List.fold_left (fun sum discussion -> 
