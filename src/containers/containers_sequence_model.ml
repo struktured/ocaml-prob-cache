eae00fa782156b16be2a33b5a5a946992df6eb2e
Carmelo Piccione <carmelo.piccione@gmail.com>
2016-08-02 23:25:13 -0400

    containers cache impl compiles again

--- src/containers/containers_sequence_model.ml
+++ src/containers/containers_sequence_model.ml
@@ -10,15 +10,14 @@ struct
   module Events : EVENTS with module Event = Event =
   struct
     module Event = Event
-    module M = Model_impl.Sequence.Make(Event)
+    module M = Containers_model_impl.Sequence.Make(Event)
     include (M : module type of M with module Event := Event)
   end
   module Event = Events.Event
   module Or_error = Containers_model_intf.Or_error
-  module M = Model_impl.Make(Events)(Or_error)
+  module M = Containers_model_impl.Make(Events)(Or_error)
   include (M : module type of M with
           module Events := Events and
           module Or_error := Or_error and
           module Event := Event)
 end
-
