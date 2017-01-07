366fd3828b53925156935f1bc8a95855655aa54d
carm <carmelo.piccione@gmail.com>
2017-01-07 17:20:28 -0500

    fix riak seq model

--- src/riak/riak_sequence_model.ml
+++ src/riak/riak_sequence_model.ml
@@ -4,6 +4,19 @@ module type EVENTS = Riak_model_intf.EVENTS
 
 module type S = Riak_model_intf.S
 
-module Make(Event:EVENT) =
-  Riak_model_impl.Make_for_events(Riak_model_impl.Make_event_sequence(Event))
-
+module Make(Event:EVENT) : S with module Events.Event = Event =
+struct
+  module Events : EVENTS with module Event = Event =
+  struct
+    module Event = Event
+    module M = Riak_model_impl.Sequence.Make(Event)
+    include (M : module type of M with module Event := Event)
+  end
+  module Event = Events.Event
+  module Or_error = Riak_model_intf.Or_error
+  module M = Riak_model_impl.Make(Events)
+  include (M : module type of M with
+          module Events := Events and
+          module Or_error := Or_error and
+          module Event := Event)
+end
